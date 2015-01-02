674fac679201a483d251ea0f5ee629d43596fba3
Ohad Manor <omanor@gmail.com>
2015-01-02 12:26:11 -0800

    Updated files

--- test_musicc.py
+++ test_musicc.py
@@ -13,19 +13,20 @@ import pandas as pd
 
 class MUSiCCTestCase(unittest.TestCase):
     """Tests for `musicc.py`."""
+    path_to_data = os.path.dirname(musicc.__file__)
 
     def test_is_output_correct_for_normalization_only(self):
         """Does MUSiCC produce the correct output for normalization of the example case?"""
-        print(os.path.dirname(musicc.__file__))
         # define the arguments needed by MUSiCC
-        musicc_args = {'input_file': 'examples/simulated_ko_relative_abundance.tab',
-                       'output_file': 'examples/test1.tab', 'input_format': 'tab', 'output_format': 'tab', 'musicc_inter': True,
+        musicc_args = {'input_file': MUSiCCTestCase.path_to_data + '/examples/simulated_ko_relative_abundance.tab',
+                       'output_file': MUSiCCTestCase.path_to_data + '/examples/test1.tab',
+                       'input_format': 'tab', 'output_format': 'tab', 'musicc_inter': True,
                        'musicc_intra': 'None', 'compute_scores': True, 'verbose': False}
         # run the MUSiCC correction
         musicc.main(musicc_args)
         # assert that the result is equal to the example (up to small difference due to OS/Other)
-        example = pd.read_table('examples/simulated_ko_MUSiCC_Normalized.tab', index_col=0)
-        output = pd.read_table('examples/test1.tab', index_col=0)
+        example = pd.read_table(MUSiCCTestCase.path_to_data + '/examples/simulated_ko_MUSiCC_Normalized.tab', index_col=0)
+        output = pd.read_table(MUSiCCTestCase.path_to_data + '/examples/test1.tab', index_col=0)
         example_vals = example.values
         output_vals = output.values
         self.assertTrue(example_vals.shape[0] == output_vals.shape[0])
@@ -34,19 +35,20 @@ class MUSiCCTestCase(unittest.TestCase):
             for j in range(example_vals.shape[1]):
                 self.assertTrue(abs(example_vals[i, j] - output_vals[i, j]) < 1)
 
-        os.remove('examples/test1.tab')
+        os.remove(MUSiCCTestCase.path_to_data + '/examples/test1.tab')
 
     def test_is_output_correct_for_normalization_correction_use_generic(self):
         """Does MUSiCC produce the correct output for normalization and correction of the example case?"""
         # define the arguments needed by MUSiCC
-        musicc_args = {'input_file': 'examples/simulated_ko_relative_abundance.tab',
-                       'output_file': 'examples/test2.tab', 'input_format': 'tab', 'output_format': 'tab', 'musicc_inter': True,
+        musicc_args = {'input_file': MUSiCCTestCase.path_to_data + '/examples/simulated_ko_relative_abundance.tab',
+                       'output_file': MUSiCCTestCase.path_to_data + '/examples/test2.tab',
+                       'input_format': 'tab', 'output_format': 'tab', 'musicc_inter': True,
                        'musicc_intra': 'use_generic', 'compute_scores': True, 'verbose': False}
         # run the MUSiCC correction
         musicc.main(musicc_args)
         # assert that the result is equal to the example (up to small difference due to OS/Other)
-        example = pd.read_table('examples/simulated_ko_MUSiCC_Normalized_Corrected_use_generic.tab', index_col=0)
-        output = pd.read_table('examples/test2.tab', index_col=0)
+        example = pd.read_table(MUSiCCTestCase.path_to_data + '/examples/simulated_ko_MUSiCC_Normalized_Corrected_use_generic.tab', index_col=0)
+        output = pd.read_table(MUSiCCTestCase.path_to_data + '/examples/test2.tab', index_col=0)
         example_vals = example.values
         output_vals = output.values
         self.assertTrue(example_vals.shape[0] == output_vals.shape[0])
@@ -55,19 +57,20 @@ class MUSiCCTestCase(unittest.TestCase):
             for j in range(example_vals.shape[1]):
                 self.assertTrue(abs(example_vals[i, j] - output_vals[i, j]) < 1)
 
-        os.remove('examples/test2.tab')
+        os.remove(MUSiCCTestCase.path_to_data + '/examples/test2.tab')
 
     def test_is_output_correct_for_normalization_correction_learn_model(self):
         """Does MUSiCC produce the correct output for normalization and correction of the example case?"""
         # define the arguments needed by MUSiCC
-        musicc_args = {'input_file': 'examples/simulated_ko_relative_abundance.tab',
-                       'output_file': 'examples/test3.tab', 'input_format': 'tab', 'output_format': 'tab', 'musicc_inter': True,
+        musicc_args = {'input_file': MUSiCCTestCase.path_to_data + '/examples/simulated_ko_relative_abundance.tab',
+                       'output_file': MUSiCCTestCase.path_to_data + '/examples/test3.tab',
+                       'input_format': 'tab', 'output_format': 'tab', 'musicc_inter': True,
                        'musicc_intra': 'learn_model', 'compute_scores': True, 'verbose': False}
         # run the MUSiCC correction
         musicc.main(musicc_args)
         # assert that the result is equal to the example (up to small difference due to de novo learning)
-        example = pd.read_table('examples/simulated_ko_MUSiCC_Normalized_Corrected_learn_model.tab', index_col=0)
-        output = pd.read_table('examples/test3.tab', index_col=0)
+        example = pd.read_table(MUSiCCTestCase.path_to_data + '/examples/simulated_ko_MUSiCC_Normalized_Corrected_learn_model.tab', index_col=0)
+        output = pd.read_table(MUSiCCTestCase.path_to_data + '/examples/test3.tab', index_col=0)
         example_vals = example.values
         output_vals = output.values
         self.assertTrue(example_vals.shape[0] == output_vals.shape[0])
@@ -76,7 +79,7 @@ class MUSiCCTestCase(unittest.TestCase):
             for j in range(example_vals.shape[1]):
                 self.assertTrue(abs(example_vals[i, j] - output_vals[i, j]) < 1)
 
-        os.remove('examples/test3.tab')
+        os.remove(MUSiCCTestCase.path_to_data + '/examples/test3.tab')
 
 ################################################
 
