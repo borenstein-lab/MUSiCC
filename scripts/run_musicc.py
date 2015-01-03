779d8a9729875747e17eb525464ff99bc46814fb
Ohad Manor <omanor@gmail.com>
2015-01-02 16:19:20 -0800

    Updated files

--- scripts/run_musicc.py
+++ scripts/run_musicc.py
@@ -2,6 +2,20 @@
 import musicc
 
 if __name__ == "__main__":
-    musicc.run_from_console()
+    # get options from user
+    parser = argparse.ArgumentParser(description='MUSiCC: Metagenomic Universal Single-Copy Correction')
+    parser.add_argument('input_file', help='Input abundance file to correct')
+    parser.add_argument('-o', '--out', dest='output_file', help='Output destination for corrected abundance (default: MUSiCC.tab)', default='MUSiCC.tab')
+    parser.add_argument('-if', '--input_format', dest='input_format', choices=['tab', 'csv', 'biom'], help='Option indicating the format of the input file (default: tab)', default='tab')
+    parser.add_argument('-of', '--output_format', dest='output_format', choices=['tab', 'csv', 'biom'], help='Option indicating the format of the output file (default: tab)', default='tab')
+    parser.add_argument('-n', '--normalize', dest='musicc_inter', help='Apply MUSiCC normalization (default: false)', action='store_true')
+    parser.add_argument('-c', '--correct', dest='musicc_intra', choices=['use_generic', 'learn_model'], help='Correct abundance per-sample using MUSiCC (default: false)', default='None')
+    parser.add_argument('-perf', '--performance', dest='compute_scores', help='Calculate model performance on various gene sets (may add to running time) (default: false)', action='store_true')
+    parser.add_argument('-v', '--verbose', dest='verbose', help='Increase verbosity of module (default: false)', action='store_true')
+
+    given_args = parser.parse_args()
+
+    # run normalization and correction
+    musicc.main(vars(given_args))
 
 
--- setup.py
+++ setup.py
@@ -24,7 +24,6 @@ setup(name='MUSiCC',
       packages=['musicc'],
       package_data={'musicc': ['data/*.tab', 'data/*.lst', 'examples/*.tab']},
       install_requires=['NumPy >= 1.6.1', 'SciPy >= 0.9', 'scikit-learn >= 0.15.2', 'pandas >= 0.14'],
-      provides=['musicc'],
       scripts=['scripts/run_musicc.py', 'tests/test_musicc.py'],
       )
 
